96d01178dec85f28a85e5ef1f5f356b0df560649
JosipHarambasic <harambasic.josip97@gmail.com>
2021-09-10 11:43:15 +0200

    completed the seal example need a README

--- src/Main.py
+++ src/Main.py
@@ -7,24 +7,31 @@ import Position
 import SealDocument
 import User
 import VisualSignature
-"""Need this class to encode our Document Object into a JSON string"""
-class DocumentEncoder(JSONEncoder):
-    def default(self, o):
-        return o.__dict__
+
 
 def main():
+    """Set the User credentials username and api-key"""
     userLogin = User.UserCredentials("api_demo_skribble_d901_0", "118d6d49-1415-4f8e-bd16-2a0ef03beaf9")
+
+    """create the token"""
     token = userLogin.createToken()
-    print(token)
+
+    """give the full path of the pdf file that you want to seal"""
     content = encodeToBase64("C:\\Users\\41786\\OneDrive\\Desktop\\FormField.pdf")
+
+    """make a visual signature where and how the seal should look like"""
     visual_signature = VisualSignature.VisualSignature(Position.Position(20,40,100,100,"0"))
-    account_name = "ais_demo_seal"
-    document = Document.Document(content, account_name, visual_signature)
-    documentAsJSON = DocumentEncoder().encode(document)
-    docm = json.dumps(document, indent=4, cls=DocumentEncoder)
-    print(docm)
+
+    """create the document with all it needs for the API call"""
+    document = Document.Document(content, "ais_demo_seal", visual_signature)
+
+    """transform the Document into a JSON file"""
+    documentAsString = DocumentEncoder().encode(document)
+    documentAsJSON = json.loads(documentAsString)
+
+    """seal the document and get the Document ID so we can do further operations with it"""
     seal = SealDocument.SealDocument()
-    response = seal.seal(token,content)
+    response = seal.seal(token,documentAsJSON)
     print(response)
 
 
@@ -32,6 +39,11 @@ def main():
 def encodeToBase64(filepath):
     with open(filepath, "rb") as pdf_file:
         return base64.b64encode(pdf_file.read()).decode("utf-8")
+
+"""Need this class to encode our Document Object into a JSON string"""
+class DocumentEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
--- src/SealDocument.py
+++ src/SealDocument.py
@@ -6,24 +6,9 @@ class SealDocument:
     def __init__(self):
         return
 
-    def seal(self, token, content):
-
-        data = {"content": content,
-                "account_name":"ais_demo_seal",
-                "visual_signature":{
-                    "position":{
-                        "x":20,
-                        "y":20,
-                        "width":100,
-                        "height":100,
-                        "page":"0"
-                    }
-                }}
-        header = {"Authorization": "Bearer" + token}
-        response = post("https://api.scribital.com/v1/seal", json=data, headers=header)
-        print(data)
+    def seal(self, token, doc):
+        
+        header = {"Authorization": "Bearer" + token,'Content-type': 'application/json'}
+        response = post("https://api.scribital.com/v1/seal", json=doc, headers=header)
         return response.content
 
-        """header = {"Authorization": "Bearer" + token}
-        response = get("https://api.scribital.com/v1/seal", json=documentAsJSON, headers=header)
-        return response.content"""
